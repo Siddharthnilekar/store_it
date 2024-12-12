ec71343e522c4789e4f9581f3c76bf93f47504aa
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2024-12-13 00:36:39 +0530

    Reusable Authform

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,7 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignUp = () => {
-  return (
-    <div>Sign Up</div>
-  )
-}
+const SignUp = () => <AuthForm type="sign-up" />;
+
 
 export default SignUp
