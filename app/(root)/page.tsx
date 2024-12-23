b8fb92ed917173c10b5ca159ee7b82ee7b2d4e47
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2024-12-23 15:17:05 +0530

    Fake Uploading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex-center h-screen">
-      <h1 className="h1">StoreIt - the only storage solution you neesd.</h1>
+      <h1 className="h1">StoreIt - the only storage solution you need.</h1>
     </div>
   );
 }
